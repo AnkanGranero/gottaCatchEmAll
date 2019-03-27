c27dce3f7e357fc3a1e06ec742aafa22b9c4eeb5
Andreas <grannemannen@gmail.com>
2019-03-27 17:25:07 +0100

    Updates

---
+++ precache-manifest.2fcb1026d725fc0d36c8c1e4172f3d87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "945662858615e351bba8",
+    "url": "/static/js/main.bf48b2fe.chunk.js"
+  },
+  {
+    "revision": "2f74d88d3345e9869d0e",
+    "url": "/static/js/2.32cb5d97.chunk.js"
+  },
+  {
+    "revision": "945662858615e351bba8",
+    "url": "/static/css/main.edfee15a.chunk.css"
+  },
+  {
+    "revision": "cf38f0afd6a5a3a257b58368979ad28b",
+    "url": "/index.html"
+  }
+];
